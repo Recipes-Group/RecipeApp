806495f7abfb37f8db101a1d107bf03f6ee09fdf
GriffinMichalak <81431781+GriffinMichalak@users.noreply.github.com>
2021-04-13 13:10:22 -0400

    Hi

--- app/src/main/java/com/example/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String recipeName;
+    private String url;
+    private ArrayList<String> steps = new ArrayList<String>();
+    private ArrayList<String> ingredients = new ArrayList<String>();
+    private int numUserInput;
+    private boolean microphoneOn = false;
+    private int step = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
+
 }
