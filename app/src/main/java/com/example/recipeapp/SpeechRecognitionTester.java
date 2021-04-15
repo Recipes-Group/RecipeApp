51688baac7299743acb99687b2d16ba8b21f7fac
Shreya Majumdar <shreya.jui.majumdar@gmail.com>
2021-04-15 10:02:48 -0400

    Began to implement speech recogniton permissions, but there are issues on lines 32, 33, and 44

--- app/src/main/java/com/example/recipeapp/SpeechRecognitionTester.java
+++ app/src/main/java/com/example/recipeapp/SpeechRecognitionTester.java
@@ -1,8 +1,19 @@
 package com.example.recipeapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.Locale;
 
 public class SpeechRecognitionTester extends AppCompatActivity {
 
@@ -10,5 +21,29 @@ public class SpeechRecognitionTester extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Checks if audio permission is granted
+        //Source: https://medium.com/voice-tech-podcast/android-speech-to-text-tutorial-8f6fa71606ac
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            checkPermission();
+        }
+
+        //Lines 32 and 33 are not being understood
+        //SpeechRecognizer#createSpeechRecognizer(Context);
+        //speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
     }
+
+    //A method to request permission
+    //Source: https://medium.com/voice-tech-podcast/android-speech-to-text-tutorial-8f6fa71606ac
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //Note that in line 44, is not recognizing RecordAudioRequestCode
+            //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, RecordAudioRequestCode);
+        }
+    }
+
+
 }
