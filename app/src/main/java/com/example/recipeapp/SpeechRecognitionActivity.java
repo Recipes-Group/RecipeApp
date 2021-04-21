3c027c17e685434e03f312d8051ded0af5e41925
Shreya Majumdar <shreya.jui.majumdar@gmail.com>
2021-04-21 12:58:15 -0400

    Add methods

---
+++ app/src/main/java/com/example/recipeapp/SpeechRecognitionActivity.java
@@ -0,0 +1,92 @@
+package com.example.recipeapp;
+
+//Code source (xml and tester): https://www.tutorialspoint.com/how-to-develop-a-speech-recognizer-in-android-without-google-api
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.RecognitionListener;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import java.util.ArrayList;
+import java.util.Locale;
+
+
+public class SpeechRecognitionActivity extends AppCompatActivity  {
+
+    public static final int REQUEST_CODE_SPEECH_INPUT = 1000;
+    //views from activity
+    TextView mTextTv;
+    ImageButton mVoiceBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.speech_recognition_tester);
+
+        mTextTv = findViewById(R.id.textTv);
+        mVoiceBtn = findViewById(R.id.voiceBtn);
+
+        //button click to show speech to text dialog
+        mVoiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
+            }
+        });
+    }
+
+    private void speak() {
+        //intent to show speech to text dialog
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi speak something");
+
+        //start intent
+        try {
+            //in there was no error
+            //show dialog
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+        }
+        catch (Exception e) {
+            //if there was some error
+            //get message of error and show
+            Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //receive voice input and handle it
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode) {
+            case REQUEST_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null!=data){
+                    //get text array from voice intent
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    //set to text view
+                    mTextTv.setText(result.get(0));
+                    break;
+                }
+            }
+
+        }
+    }
+}
